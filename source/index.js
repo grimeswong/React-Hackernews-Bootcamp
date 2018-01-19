8afe9eda8b22bf416b05835adb4a092bb95bcbba
Shadow Grimes <grimeswong@gmail.com>
2018-01-19 16:08:35 +1000

    Chapter-0: Added root element to display text with JSX and pureJS syntax

--- source/index.js
+++ source/index.js
@@ -1,6 +1,25 @@
-  document.body.innerHTML =
-    `
-      <div>
-        Hello Hacker News
-      </div>
-    `;
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+/* Pure javascript Syntax*/
+// const element = React.createElement(
+//   'div', // the type of element we're creating
+//   null, // any props ('attributes') we're giving it
+//   'Hello Hacker News' // The children of this element.
+// );
+
+/* JSX Syntax */
+const element =
+  <div>
+  Hello Hacker News
+  </div>
+
+const myNum = React.createElement(
+  'div',
+  {dataTest: 10},
+  'Hello Hacker News'
+)
+
+const root = document.getElementById('root');
+
+ReactDOM.render(myNum, root);
