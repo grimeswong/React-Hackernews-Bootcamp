2b56c8d0223656c32ab5fb6bc40ae705d6a767a8
Shadow Grimes <grimeswong@gmail.com>
2018-01-20 11:11:41 +1000

    Chapter-2: Added the styles for title, description and link

--- source/Component/Article/index.jsx
+++ source/Component/Article/index.jsx
@@ -5,11 +5,10 @@ import styles from './styles.css';  // import css file from the style sheet from
 export default function Article(props) {  // components that start with a lower letter have their name converted into a string (when compiled)
   return (
     <div>
-      <h2>Title: {props.title}</h2>
-      <p>{props.description}
-        <a href={props.link}> {props.link}</a>
+      <h2 className={styles.title}>Title: {props.title}</h2>
+      <p className={styles.description}>{props.description}
+        <a className={styles.link}href={props.link}> {props.link}</a>
       </p>
-
     </div>
   );
 }
