e1872cfb0034ecc8e80931ebba7f52fa60d2d2ed
Shadow Grimes <grimeswong@gmail.com>
2018-01-20 11:03:32 +1000

    Chapter-2: Added style sheet and changed h2 color

--- source/Component/Article/index.jsx
+++ source/Component/Article/index.jsx
@@ -1,12 +1,15 @@
 // Path: source/Component/Article/index.jsx
 import React from 'react';
+import styles from './styles.css';  // import css file from the style sheet from the same level
 
 export default function Article(props) {  // components that start with a lower letter have their name converted into a string (when compiled)
   return (
     <div>
       <h2>Title: {props.title}</h2>
-      <p>{props.description}</p>
-      <a href={props.link}>{props.link}</a>
+      <p>{props.description}
+        <a href={props.link}> {props.link}</a>
+      </p>
+
     </div>
   );
 }
