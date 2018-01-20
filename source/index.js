ae14224eceadb56a009f00f3eea69b65390383bb
Shadow Grimes <grimeswong@gmail.com>
2018-01-20 10:31:48 +1000

    Chapter-1: Added the list of articles with link and title

--- source/index.js
+++ source/index.js
@@ -26,17 +26,32 @@ function Article(props) {  // components that start with a lower letter have the
     <div>
       <h2>Title: {props.title}</h2>
       <p>{props.description}</p>
-      <a href={props.link}>Link</a>
+      <a href={props.link}>{props.link}</a>
     </div>
   );
 }
 
+// Fake message
+const message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+
 const article =
+<div>
   <Article
     title="Hello Hacker News"
-    description="Bar Bar Bar"
+    description= {message}
+    link="https://news.ycombinator.com/item?id=15806500"
+  />
+  <Article
+    title="Tech News"
+    description={message}
+    link="http://www.couriermail.com.au/technology?nk=e807bdccff5d537cd751655ae3756ef5-1516408216"
+  />
+  <Article
+    title="Yahoo News"
+    description={message}
     link="http://yahoo.com.au"
   />
+</div>
 
 
 const root = document.getElementById('root');
