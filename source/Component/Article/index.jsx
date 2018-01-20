11e85e8b1f35607edb9a2edbbb0f8b21506a9e6a
Shadow Grimes <grimeswong@gmail.com>
2018-01-20 12:16:49 +1000

    Chapter-2: Added article in the view

--- source/Component/Article/index.jsx
+++ source/Component/Article/index.jsx
@@ -5,9 +5,9 @@ import styles from './styles.css';  // import css file from the style sheet from
 export default function Article(props) {  // components that start with a lower letter have their name converted into a string (when compiled)
   return (
     <div>
-      <h2 className={styles.title}>Title: {props.title}</h2>
-      <p className={styles.description}>{props.description}
-        <a className={styles.link}href={props.link}> {props.link}</a>
+      <h2 className={styles.title}>Title: {props.article.title}</h2>
+      <p className={styles.description}>{props.article.description}
+        <a className={styles.link}href={props.link}> {props.article.link}</a>
       </p>
     </div>
   );
--- source/index.js
+++ source/index.js
@@ -1,30 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Article from './Component/Article';
+import Article from './Component/Article/'
 
-const message = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
-const article =
-<div>
-  <Article
-    title="Hello Hacker News"
-    description= {message}
-    link="https://news.ycombinator.com/item?id=15806500"
-  />
-  <Article
-    title="Tech News"
-    description={message}
-    link="http://www.couriermail.com.au/technology?nk=e807bdccff5d537cd751655ae3756ef5-1516408216"
-  />
-  <Article
-    title="Yahoo News"
-    description={message}
-    link="http://yahoo.com.au"
-  />
-</div>
+
+const LOREM_IPSUM =
+  `
+  Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+  Lorem Ipsum has been the industry's standard dummy text ever since the
+  1500s, when an unknown printer took a galley of type and scrambled it to
+  make a type specimen book.`
+
+const ARTICLES = [
+  {
+    description: LOREM_IPSUM,
+    id: 1,
+    link: 'https://google.com',
+    title: 'Article 1',
+  },
+  {
+    description: LOREM_IPSUM,
+    id: 2,
+    link: 'https://google.com',
+    title: 'Article 2',
+  },
+  {
+    description: LOREM_IPSUM,
+    id: 3,
+    link: 'https://google.com',
+    title: 'Article 3',
+  },
+]
+
+const Views =
+  <div>
+    <Article article={ARTICLES[0]} /> 
+    <Article article={ARTICLES[1]} />
+    <Article article={ARTICLES[2]} />
+  </div>
 
 
 const root = document.getElementById('root');
 
-ReactDOM.render(article, root);
+ReactDOM.render(Views, root);
